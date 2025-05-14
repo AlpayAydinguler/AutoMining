28f3c51a7a77af4878e113200e529aa1ec6bd308
Alpay Aydıngüler <alpayaydinguler@gmail.com>
2025-05-14 18:20:23 +0300

    Added docking after mining finishes

--- AutoMining/Form1.Designer.cs
+++ AutoMining/Form1.Designer.cs
@@ -31,7 +31,7 @@
             labelLiveMouseLocation = new Label();
             buttonStartStopButton = new Button();
             textBoxBox1TopLeftX = new TextBox();
-            labelBox1CornerLocations = new Label();
+            labelInventoryBoxCornerLocations = new Label();
             labelBox1TopLeftX = new Label();
             labelBox1TopLeftY = new Label();
             textBoxBox1TopLeftY = new TextBox();
@@ -44,13 +44,27 @@
             labelLiveMouseLocationLabel = new Label();
             labelStatusLabel = new Label();
             LabelStatus = new Label();
+            labelMiningStatusLabel = new Label();
+            labelMiningStatus = new Label();
+            label1 = new Label();
+            textBoxMiningModuleX = new TextBox();
+            textBoxMiningModuleY = new TextBox();
+            label2 = new Label();
+            textBoxDockButtonTopLeftX = new TextBox();
+            textBoxDockButtonTopLeftY = new TextBox();
+            textBoxDockButtonBottomRightX = new TextBox();
+            textBoxDockButtonBottomRightY = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
             SuspendLayout();
             // 
             // labelLiveMouseLocation
             // 
             labelLiveMouseLocation.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
             labelLiveMouseLocation.AutoSize = true;
-            labelLiveMouseLocation.Location = new Point(143, 137);
+            labelLiveMouseLocation.Location = new Point(143, 225);
             labelLiveMouseLocation.Name = "labelLiveMouseLocation";
             labelLiveMouseLocation.Size = new Size(24, 15);
             labelLiveMouseLocation.TabIndex = 0;
@@ -59,7 +73,7 @@
             // buttonStartStopButton
             // 
             buttonStartStopButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-            buttonStartStopButton.Location = new Point(406, 126);
+            buttonStartStopButton.Location = new Point(487, 214);
             buttonStartStopButton.Name = "buttonStartStopButton";
             buttonStartStopButton.Size = new Size(75, 23);
             buttonStartStopButton.TabIndex = 1;
@@ -69,20 +83,20 @@
             // 
             // textBoxBox1TopLeftX
             // 
-            textBoxBox1TopLeftX.Location = new Point(115, 27);
+            textBoxBox1TopLeftX.Location = new Point(147, 27);
             textBoxBox1TopLeftX.Name = "textBoxBox1TopLeftX";
             textBoxBox1TopLeftX.Size = new Size(100, 23);
             textBoxBox1TopLeftX.TabIndex = 2;
             textBoxBox1TopLeftX.Text = "220";
             // 
-            // labelBox1CornerLocations
+            // labelInventoryBoxCornerLocations
             // 
-            labelBox1CornerLocations.AutoSize = true;
-            labelBox1CornerLocations.Location = new Point(12, 9);
-            labelBox1CornerLocations.Name = "labelBox1CornerLocations";
-            labelBox1CornerLocations.Size = new Size(133, 15);
-            labelBox1CornerLocations.TabIndex = 3;
-            labelBox1CornerLocations.Text = "Box 1 corner locations : ";
+            labelInventoryBoxCornerLocations.AutoSize = true;
+            labelInventoryBoxCornerLocations.Location = new Point(12, 9);
+            labelInventoryBoxCornerLocations.Name = "labelInventoryBoxCornerLocations";
+            labelInventoryBoxCornerLocations.Size = new Size(177, 15);
+            labelInventoryBoxCornerLocations.TabIndex = 3;
+            labelInventoryBoxCornerLocations.Text = "Inventory Box corner locations : ";
             // 
             // labelBox1TopLeftX
             // 
@@ -96,7 +110,7 @@
             // labelBox1TopLeftY
             // 
             labelBox1TopLeftY.AutoSize = true;
-            labelBox1TopLeftY.Location = new Point(221, 30);
+            labelBox1TopLeftY.Location = new Point(253, 30);
             labelBox1TopLeftY.Name = "labelBox1TopLeftY";
             labelBox1TopLeftY.Size = new Size(97, 15);
             labelBox1TopLeftY.TabIndex = 5;
@@ -104,7 +118,7 @@
             // 
             // textBoxBox1TopLeftY
             // 
-            textBoxBox1TopLeftY.Location = new Point(324, 27);
+            textBoxBox1TopLeftY.Location = new Point(388, 27);
             textBoxBox1TopLeftY.Name = "textBoxBox1TopLeftY";
             textBoxBox1TopLeftY.Size = new Size(100, 23);
             textBoxBox1TopLeftY.TabIndex = 7;
@@ -159,13 +173,13 @@
             textBoxLoopInterval.Name = "textBoxLoopInterval";
             textBoxLoopInterval.Size = new Size(100, 23);
             textBoxLoopInterval.TabIndex = 22;
-            textBoxLoopInterval.Text = "96,3";
+            textBoxLoopInterval.Text = "96.3";
             // 
             // labelLiveMouseLocationLabel
             // 
             labelLiveMouseLocationLabel.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
             labelLiveMouseLocationLabel.AutoSize = true;
-            labelLiveMouseLocationLabel.Location = new Point(12, 137);
+            labelLiveMouseLocationLabel.Location = new Point(12, 225);
             labelLiveMouseLocationLabel.Name = "labelLiveMouseLocationLabel";
             labelLiveMouseLocationLabel.Size = new Size(125, 15);
             labelLiveMouseLocationLabel.TabIndex = 23;
@@ -175,7 +189,7 @@
             // 
             labelStatusLabel.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
             labelStatusLabel.AutoSize = true;
-            labelStatusLabel.Location = new Point(12, 122);
+            labelStatusLabel.Location = new Point(12, 210);
             labelStatusLabel.Name = "labelStatusLabel";
             labelStatusLabel.Size = new Size(48, 15);
             labelStatusLabel.TabIndex = 24;
@@ -185,17 +199,151 @@
             // 
             LabelStatus.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
             LabelStatus.AutoSize = true;
-            LabelStatus.Location = new Point(66, 122);
+            LabelStatus.Location = new Point(66, 210);
             LabelStatus.Name = "LabelStatus";
             LabelStatus.Size = new Size(38, 15);
             LabelStatus.TabIndex = 25;
             LabelStatus.Text = "label1";
             // 
+            // labelMiningStatusLabel
+            // 
+            labelMiningStatusLabel.AutoSize = true;
+            labelMiningStatusLabel.Location = new Point(12, 285);
+            labelMiningStatusLabel.Name = "labelMiningStatusLabel";
+            labelMiningStatusLabel.Size = new Size(89, 15);
+            labelMiningStatusLabel.TabIndex = 26;
+            labelMiningStatusLabel.Text = "Mining Status : ";
+            // 
+            // labelMiningStatus
+            // 
+            labelMiningStatus.AutoSize = true;
+            labelMiningStatus.Location = new Point(107, 285);
+            labelMiningStatus.Name = "labelMiningStatus";
+            labelMiningStatus.Size = new Size(38, 15);
+            labelMiningStatus.TabIndex = 27;
+            labelMiningStatus.Text = "label1";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 117);
+            label1.Name = "label1";
+            label1.Size = new Size(108, 15);
+            label1.TabIndex = 28;
+            label1.Text = "Mining Module X : ";
+            // 
+            // textBoxMiningModuleX
+            // 
+            textBoxMiningModuleX.Location = new Point(126, 114);
+            textBoxMiningModuleX.Name = "textBoxMiningModuleX";
+            textBoxMiningModuleX.Size = new Size(100, 23);
+            textBoxMiningModuleX.TabIndex = 29;
+            textBoxMiningModuleX.Text = "1366";
+            // 
+            // textBoxMiningModuleY
+            // 
+            textBoxMiningModuleY.Location = new Point(346, 114);
+            textBoxMiningModuleY.Name = "textBoxMiningModuleY";
+            textBoxMiningModuleY.Size = new Size(100, 23);
+            textBoxMiningModuleY.TabIndex = 30;
+            textBoxMiningModuleY.Text = "1265";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(232, 117);
+            label2.Name = "label2";
+            label2.Size = new Size(108, 15);
+            label2.TabIndex = 31;
+            label2.Text = "Mining Module Y : ";
+            // 
+            // textBoxDockButtonTopLeftX
+            // 
+            textBoxDockButtonTopLeftX.Location = new Point(184, 143);
+            textBoxDockButtonTopLeftX.Name = "textBoxDockButtonTopLeftX";
+            textBoxDockButtonTopLeftX.Size = new Size(100, 23);
+            textBoxDockButtonTopLeftX.TabIndex = 32;
+            textBoxDockButtonTopLeftX.Text = "2140";
+            // 
+            // textBoxDockButtonTopLeftY
+            // 
+            textBoxDockButtonTopLeftY.Location = new Point(462, 143);
+            textBoxDockButtonTopLeftY.Name = "textBoxDockButtonTopLeftY";
+            textBoxDockButtonTopLeftY.Size = new Size(100, 23);
+            textBoxDockButtonTopLeftY.TabIndex = 33;
+            textBoxDockButtonTopLeftY.Text = "140";
+            // 
+            // textBoxDockButtonBottomRightX
+            // 
+            textBoxDockButtonBottomRightX.Location = new Point(184, 172);
+            textBoxDockButtonBottomRightX.Name = "textBoxDockButtonBottomRightX";
+            textBoxDockButtonBottomRightX.Size = new Size(100, 23);
+            textBoxDockButtonBottomRightX.TabIndex = 34;
+            textBoxDockButtonBottomRightX.Text = "2155";
+            // 
+            // textBoxDockButtonBottomRightY
+            // 
+            textBoxDockButtonBottomRightY.Location = new Point(462, 172);
+            textBoxDockButtonBottomRightY.Name = "textBoxDockButtonBottomRightY";
+            textBoxDockButtonBottomRightY.Size = new Size(100, 23);
+            textBoxDockButtonBottomRightY.TabIndex = 35;
+            textBoxDockButtonBottomRightY.Text = "155";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 175);
+            label3.Name = "label3";
+            label3.Size = new Size(166, 15);
+            label3.TabIndex = 36;
+            label3.Text = "Dock Button Bottom Right X : ";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(290, 175);
+            label4.Name = "label4";
+            label4.Size = new Size(166, 15);
+            label4.TabIndex = 37;
+            label4.Text = "Dock Button Bottom Right Y : ";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(12, 146);
+            label5.Name = "label5";
+            label5.Size = new Size(136, 15);
+            label5.TabIndex = 38;
+            label5.Text = "Dock Button Top Lelt X : ";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(290, 146);
+            label6.Name = "label6";
+            label6.Size = new Size(136, 15);
+            label6.TabIndex = 39;
+            label6.Text = "Dock Button Top Lelt Y : ";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(493, 161);
+            ClientSize = new Size(574, 249);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(textBoxDockButtonBottomRightY);
+            Controls.Add(textBoxDockButtonBottomRightX);
+            Controls.Add(textBoxDockButtonTopLeftY);
+            Controls.Add(textBoxDockButtonTopLeftX);
+            Controls.Add(label2);
+            Controls.Add(textBoxMiningModuleY);
+            Controls.Add(textBoxMiningModuleX);
+            Controls.Add(label1);
+            Controls.Add(labelMiningStatus);
+            Controls.Add(labelMiningStatusLabel);
             Controls.Add(LabelStatus);
             Controls.Add(labelStatusLabel);
             Controls.Add(labelLiveMouseLocationLabel);
@@ -208,7 +356,7 @@
             Controls.Add(textBoxBox1TopLeftY);
             Controls.Add(labelBox1TopLeftY);
             Controls.Add(labelBox1TopLeftX);
-            Controls.Add(labelBox1CornerLocations);
+            Controls.Add(labelInventoryBoxCornerLocations);
             Controls.Add(textBoxBox1TopLeftX);
             Controls.Add(buttonStartStopButton);
             Controls.Add(labelLiveMouseLocation);
@@ -223,7 +371,7 @@
         private Label labelLiveMouseLocation;
         private Button buttonStartStopButton;
         private TextBox textBoxBox1TopLeftX;
-        private Label labelBox1CornerLocations;
+        private Label labelInventoryBoxCornerLocations;
         private Label labelBox1TopLeftX;
         private Label labelBox1TopLeftY;
         private TextBox textBoxBox1TopLeftY;
@@ -236,5 +384,19 @@
         private Label labelLiveMouseLocationLabel;
         private Label labelStatusLabel;
         private Label LabelStatus;
+        private Label labelMiningStatusLabel;
+        private Label labelMiningStatus;
+        private Label label1;
+        private TextBox textBoxMiningModuleX;
+        private TextBox textBoxMiningModuleY;
+        private Label label2;
+        private TextBox textBoxDockButtonTopLeftX;
+        private TextBox textBoxDockButtonTopLeftY;
+        private TextBox textBoxDockButtonBottomRightX;
+        private TextBox textBoxDockButtonBottomRightY;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
     }
 }
--- AutoMining/Form1.cs
+++ AutoMining/Form1.cs
@@ -9,11 +9,23 @@ namespace AutoMining
 {
     public partial class Form1 : Form
     {
+        // Existing members
         private readonly System.Windows.Forms.Timer mouseTrackerTimer = new System.Windows.Forms.Timer();
         private readonly System.Windows.Forms.Timer actionTimer = new System.Windows.Forms.Timer();
+        private readonly System.Windows.Forms.Timer miningCheckTimer = new System.Windows.Forms.Timer();
         private bool isRunning = false;
+        private bool isMiningStopped = false;
+        private bool isDocking = false;
         private Random random = new Random();
         private Point rightClickPosition;
+        private List<Color> colorSamples = new List<Color>();
+
+        // Add these new constants
+        private const int COLOR_CHECK_INTERVAL_MIN = 1000;
+        private const int COLOR_CHECK_INTERVAL_MAX = 5000;
+        private const int COLOR_SAMPLES_COUNT = 3;
+        private const int DOCK_DELAY_MIN = 15000;
+        private const int DOCK_DELAY_MAX = 30000;
 
         [DllImport("user32.dll")]
         private static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, int dwExtraInfo);
@@ -25,20 +37,137 @@ namespace AutoMining
 
         // Context menu position offsets (modify these according to your context menu)
         private const int MenuXOffset = 407 - 390;  // Difference between right-click X and menu X
-        private const int MenuYOffset = 910-850; // Difference between right-click Y and menu Y
-        private const int MenuItemWidth = 609-465; // Width of the menu item (537-367)
-        private const int MenuItemHeight = 965-946; // Height of the menu item (834-816)
+        private const int MenuYOffset = 910 - 850; // Difference between right-click Y and menu Y
+        private const int MenuItemWidth = 609 - 465; // Width of the menu item (537-367)
+        private const int MenuItemHeight = 965 - 946; // Height of the menu item (834-816)
+
+        private const int COLOR_TOLERANCE = 10;
 
         public Form1()
         {
             InitializeComponent();
 
+            // Existing initialization
             mouseTrackerTimer.Interval = 50;
             mouseTrackerTimer.Tick += MouseTrackerTimer_Tick;
             mouseTrackerTimer.Start();
 
             actionTimer.Interval = 1000;
             actionTimer.Tick += ActionTimer_Tick;
+
+            // New mining check timer setup
+            miningCheckTimer.Tick += MiningCheckTimer_Tick;
+            ResetMiningCheckTimer();
+        }
+
+        private void ResetMiningCheckTimer()
+        {
+            miningCheckTimer.Interval = random.Next(COLOR_CHECK_INTERVAL_MIN, COLOR_CHECK_INTERVAL_MAX);
+            miningCheckTimer.Start();
+        }
+
+        private void MiningCheckTimer_Tick(object sender, EventArgs e)
+        {
+            if (!isRunning || isDocking) return;
+
+            try
+            {
+                // Get mining module coordinates
+                var modulePoint = new Point(
+                    int.Parse(textBoxMiningModuleX.Text),
+                    int.Parse(textBoxMiningModuleY.Text)
+                );
+
+                // Capture screen color
+                using (var bmp = new Bitmap(1, 1))
+                using (var g = Graphics.FromImage(bmp))
+                {
+                    g.CopyFromScreen(modulePoint, Point.Empty, new Size(1, 1));
+                    var color = bmp.GetPixel(0, 0);
+                    colorSamples.Add(color);
+
+                    // Keep only last 3 samples
+                    if (colorSamples.Count > COLOR_SAMPLES_COUNT)
+                        colorSamples.RemoveAt(0);
+
+                    UpdateMiningStatus(color);
+                }
+
+                // Check if mining stopped
+                if (colorSamples.Count == COLOR_SAMPLES_COUNT &&
+                    colorSamples.TrueForAll(c => c == colorSamples[0]))
+
+                {
+                    isMiningStopped = true;
+                    UpdateStatus("Mining stopped - initiating docking");
+                    actionTimer.Stop();
+                    InitiateDockingSequence();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Color check error: {ex.Message}");
+            }
+            finally
+            {
+                ResetMiningCheckTimer();
+            }
+        }
+
+        private void UpdateMiningStatus(Color currentColor)
+        {
+            labelMiningStatus.Invoke((MethodInvoker)(() =>
+                labelMiningStatus.Text = $"Mining Status: {(isMiningStopped ? "STOPPED" : "ACTIVE")}\n" +
+                                       $"Last Color: {currentColor.ToString()}"));
+        }
+
+        private async void InitiateDockingSequence()
+        {
+            if (isDocking) return;
+            isDocking = true;
+
+            try
+            {
+                // Stop all timers
+                miningCheckTimer.Stop();
+
+                // Press Shift+R
+                SendKeys.SendWait("+R");
+
+                // Wait random delay
+                int delay = random.Next(DOCK_DELAY_MIN, DOCK_DELAY_MAX);
+                UpdateStatus($"Waiting {delay / 1000} seconds before docking...");
+                await Task.Delay(delay);
+
+                // Click dock button
+                var dockBox = new Rectangle(
+                    int.Parse(textBoxDockButtonTopLeftX.Text),
+                    int.Parse(textBoxDockButtonTopLeftY.Text),
+                    int.Parse(textBoxDockButtonBottomRightX.Text) - int.Parse(textBoxDockButtonTopLeftX.Text),
+                    int.Parse(textBoxDockButtonBottomRightY.Text) - int.Parse(textBoxDockButtonTopLeftY.Text)
+                );
+
+                var target = GetRandomPointInRectangle(dockBox);
+                HumanLikeMouseMove(target);
+                LeftClick();
+                UpdateStatus("Docking initiated");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Docking error: {ex.Message}");
+            }
+            finally
+            {
+                isDocking = false;
+                isMiningStopped = false;
+                colorSamples.Clear();
+
+                if (isRunning)
+                {
+                    actionTimer.Start();
+                    ResetMiningCheckTimer();
+                }
+            }
         }
 
         private void MouseTrackerTimer_Tick(object sender, EventArgs e)
@@ -56,15 +185,19 @@ namespace AutoMining
 
             if (isRunning)
             {
-                // Change to double.TryParse to handle decimal values
+                colorSamples.Clear();
+                isMiningStopped = false;
+
                 if (double.TryParse(textBoxLoopInterval.Text, NumberStyles.Any, CultureInfo.InvariantCulture, out double interval) && interval > 0)
                 {
-                    actionTimer.Interval = (int)(random.Next((int)interval +1 , (int)(interval * 2) -1 ) * 1000); // Convert seconds to milliseconds
+                    actionTimer.Interval = (int)(interval * 1000);
                     actionTimer.Start();
+                    miningCheckTimer.Start();
+                    PerformAutomationSteps(); // Immediate first run
                 }
                 else
                 {
-                    MessageBox.Show("Please enter a valid positive number for seconds (decimal values allowed)");
+                    MessageBox.Show("Please enter a valid positive number for seconds");
                     isRunning = false;
                     buttonStartStopButton.Text = "Start";
                 }
@@ -72,6 +205,7 @@ namespace AutoMining
             else
             {
                 actionTimer.Stop();
+                miningCheckTimer.Stop();
             }
         }
 
@@ -151,15 +285,53 @@ namespace AutoMining
         private void HumanLikeMouseMove(Point target)
         {
             var start = Cursor.Position;
-            var steps = random.Next(20, 40);
+            var steps = random.Next(30, 60); // More steps for smoother curves
+            var baseOffset = random.Next(15, 30); // Base curve amount
+
+            // Create control points for Bézier curve
+            var control1 = new Point(
+                start.X + random.Next(-baseOffset, baseOffset),
+                start.Y + random.Next(-baseOffset, baseOffset)
+            );
+
+            var control2 = new Point(
+                target.X + random.Next(-baseOffset, baseOffset),
+                target.Y + random.Next(-baseOffset, baseOffset)
+            );
 
             for (int i = 0; i <= steps; i++)
             {
-                var x = start.X + (target.X - start.X) * i / steps;
-                var y = start.Y + (target.Y - start.Y) * i / steps;
-                Cursor.Position = new Point(x, y);
-                Thread.Sleep(random.Next(5, 15));
+                double t = (double)i / steps;
+
+                // Cubic Bézier curve calculation
+                double u = 1 - t;
+                double tt = t * t;
+                double uu = u * u;
+                double uuu = uu * u;
+                double ttt = tt * t;
+
+                // Add some randomness to the final position
+                var finalTarget = new Point(
+                    target.X + random.Next(-3, 3),
+                    target.Y + random.Next(-3, 3)
+                );
+
+                Point point = new Point(
+                    (int)(uuu * start.X + 3 * uu * t * control1.X + 3 * u * tt * control2.X + ttt * finalTarget.X),
+                    (int)(uuu * start.Y + 3 * uu * t * control1.Y + 3 * u * tt * control2.Y + ttt * finalTarget.Y)
+                );
+
+                // Add small random delays and micro-movements
+                Cursor.Position = point;
+                Thread.Sleep(random.Next(8, 20)); // Vary speed during movement
+
+                // Random tiny pauses (human-like hesitation)
+                if (random.Next(100) < 5) // 5% chance of micro-pause
+                    Thread.Sleep(random.Next(30, 80));
             }
+
+            // Final adjustment to ensure we reach exact target
+            Cursor.Position = target;
         }
 
         private void LeftClick()
@@ -179,7 +351,7 @@ namespace AutoMining
                 Invoke((MethodInvoker)(() => UpdateStatus(message)));
                 return;
             }
-            LabelStatus.Text = $"Status: {message}";
+            LabelStatus.Text = $"{message}";
         }
 
         protected override void OnFormClosing(FormClosingEventArgs e)
